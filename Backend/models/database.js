2cec26c8d9c1bcb10513fb3d41321b149c2f44d6
Kalash Mishra <mishrakalash18@gmail.com>
2023-11-06 15:59:31 +0530

    change

--- Backend/models/database.js
+++ Backend/models/database.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 exports.connectDatabase = async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL,{
+            dbName: "job-portal",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+ 
         console.log("Database connected")
     } catch (error) {
         console.log(error.message)
