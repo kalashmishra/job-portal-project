650581cc9e1bf6eaff1c6e996854e01f40f7f92d
Kalash Mishra <mishrakalash18@gmail.com>
2023-11-06 18:55:43 +0530

    change

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ require("./models/database").connectDatabase();
 
 // cors
 const cors = require("cors");
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin:["https://job-portal-project-gold.vercel.app"] }));
 // logger
 const logger = require("morgan");
 app.use(logger("tiny"));
